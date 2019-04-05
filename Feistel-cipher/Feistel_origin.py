7d2a93d4a5a4f9afe0d5c0f787cad67807dcf77d
kcorlidy <1220181099@qq.com>
2019-04-05 19:28:15 +0800

    make it more readable

--- Feistel-cipher/Feistel_origin.py
+++ Feistel-cipher/Feistel_origin.py
@@ -1,38 +1,60 @@
-key = "1234"
-plaintext = "abcd"
-strtobin = lambda st: ''.join(format(ord(x), 'b') for x in st)
+from binascii import a2b_hex, b2a_hex
+import unittest
+import re
 
-lens = int(len(plaintext)/2)
-L0 = strtobin(plaintext[:lens])
-R0 = strtobin(plaintext[lens:])
+class Feistel(object):
+	"""docstring for Fi"""
+	def __init__(self, key, rounds, f=None):
+		self.s2b = lambda st: ''.join(format(ord(x), 'b') for x in st)
+		self.key = self.s2b(key)
+		self.rounds = rounds
+		self.F = f if f else Feistel.F
 
-print("L0:",L0,"\nR0:",R0)
-i = 2
-key = strtobin(key)
 
-def F(a,key):
-	return bin(int(a, 2)+ int(key, 2))[2:]
+	def F(a,key):
+		return bin(int(a, 2) * int(key, 2))[2:]
 
-def encrypt(L0,R0,F):
-	L = [L0]
-	R = [R0]
-	for _ in range(i):	
-		L += [R[_]]
-		R += ["".join([str(int(a)^int(b)) for a,b in zip(list(L[_]),list(F(R[_],key)))])]
-		
-	return L[-1],R[-1]
+	def zip_(self,first,second):
+		return zip(list(first),list(self.F(second,self.key)))
 
-def decrypt(Ln,Rn,F):
-	L = [None]*i + [Ln]
-	R = [None]*i + [Rn]
-	for _ in range(i)[::-1]:
-		R[_] = L[_+1]
-		L[_] = "".join([str(int(a)^int(b)) for a,b in zip(list(R[_+1]),list(F(L[_+1],key)))])
-		
-	return L[0],R[0]
+	def encrypt(self,plaintext):
+		lens = int(len(plaintext)/2)
+		L = [self.s2b(plaintext[:lens])]
+		R = [self.s2b(plaintext[lens:])]
 
-Ln,Rn = encrypt(L0,R0,F)
-print(Ln,Rn)
+		for _ in range(self.rounds):	
+			L += [R[_]]
+			R += ["".join([str(int(a)^int(b)) for a,b in self.zip_(L[_],R[_])])]
+				
+		return L[-1],R[-1]
 
-L0,R0 = decrypt(Ln,Rn,F)
-print(L0,R0)
+	def decrypt(self,Ln,Rn):
+		L = [None]*self.rounds + [Ln]
+		R = [None]*self.rounds + [Rn]
+		for _ in range(self.rounds)[::-1]:
+			R[_] = L[_+1]
+			L[_] = "".join([str(int(a)^int(b)) for a,b in self.zip_(R[_+1],L[_+1])])
+				
+		return self.b2s(L[0]+R[0])
+
+	def b2s(self,bins):
+		return "".join([chr(int(ele,2)) for ele in re.findall(r"\w{7}",bins)])
+
+	def b2h(self,strs):
+		return hex(int(strs,2))
+
+class test(unittest.TestCase):
+	"""docstring for test"""
+	def test_base(self):
+		key = "1234"
+		plaintext = "abcd"
+
+		f = Feistel(key,3)
+		Ln,Rn = f.encrypt(plaintext)
+		plaintext_ = f.decrypt(Ln,Rn)
+
+		self.assertEqual(plaintext,plaintext_)
+
+if __name__ == '__main__':
+
+	unittest.main()
