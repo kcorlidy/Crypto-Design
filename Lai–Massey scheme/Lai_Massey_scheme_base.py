ede7e98a1ef21b1de7315b50a6bd068e25c0262b
kcorlidy <1220181099@qq.com>
2019-04-07 21:39:29 +0800

    im still confusing in some situation.

--- Lai–Massey scheme/Lai_Massey_scheme_base.py
+++ Lai–Massey scheme/Lai_Massey_scheme_base.py
@@ -7,76 +7,150 @@ from Crypto import Random
 
 class Lai_Massey(object):
 	
-	def __init__(self, key, rounds, f=None, h=None):
-		self.key = self.all2int(key)
+	def __init__(self, key, rounds, f=None, h=None, h_=None):
+		self.key = self.s2b(key)
 		self.rounds = rounds
-		self.F = f if f else Lai_Massey.F
-		self.H = h if h else Lai_Massey.H
+		self.F = f if f else self.F
+		self.H = h if h else self.H
+		self.H_ = h_ if h_ else self.H_
 		self.checkit(key)
 
 	
 	def checkit(self,key):
-		if self.F != Lai_Massey.F:
+		if self.F != self.F:
 			warnings.warn("You are using self-define F function, which have to return a binary string", ParamWarning,stacklevel=2)
 		if len(key)%2 != 0:
 			raise ParamError("Invalid key length")
 
-	def all2int(self,value):
+	def s2b(self,value):
 		try:
-			return int(''.join(format(ord(x), '#010b')[2:] for x in value), 2) # 8bit is necessary.
+			return "".join([format(ord(x), '#010b')[2:] for x in value]) # 8bit is necessary.
 		except Exception as e:
 			try:
-				return self.all2int(hexlify(value).decode())
+				return self.s2b(hexlify(value).decode())
 			except Exception as e:
 				pass
 
 		raise ParamError("Can't decode your input.")
 
-	def F(a,key):
-		return pow(a * key,2)
+	def F(self,a,key):
+		# F need to return a integer that can ensure every number is positive.
+		p = a*key
+		return p if p>0 else -p
 
-	def H(L,R,T=None):
-		return L,R
 
+	def H(self,L,R):
+		# If you did different process to L compare to R,
+		#	 so you will drawn into a complicated math question.
+		#	 because that time H_ is not H^(-1).
+		# To 2019-04-07 20:58:26, i test + - * / ^ << >>. Found (+,-) and (<<,>>) and (^) can finish it simple.
+		# Matrix maybe possible. because `offset`. 
+		'''test'''
+		#return L + 20, R + 20 # work -1
+		#return L + self.key,R + self.key # work -1 extra
+		#return L << 2, R << 2 # work -2
+		return L ^ self.key,R ^ self.key # work -3
+		#return L,R
+		
+
+	def H_(self,L,R):
+		'''test'''
+		#return L - 40, R - 40 # work -1
+		#return L - 2*self.key,R - 2*self.key # work -1 extra
+		#return L >> 4, R >> 4 # work -2
+		return L ^ self.key,R ^ self.key # work -3
+		#return L,R
+		
 
 	def encrypt(self,plaintext):
 		lens = int(len(plaintext)/2)
-		L = self.all2int(plaintext[:lens])
-		R = self.all2int(plaintext[lens:])
-
+		L = self.s2b(plaintext[:lens])
+		R = self.s2b(plaintext[lens:])
 		# R0' = R_0, L0' = L0
-		L_, R_ = self.H(L,R)
+
+		L_, R_, self.key = int(L,2), int(R,2), int(self.key, 2)
+		
+		L_, R_ = self.H(L_, R_)
 		for _ in range(self.rounds):
-			T = self.F(L_ - R_, self.key)
+			T = self.F(L_- R_, self.key)
 			L_, R_ = self.H(L_ + T, R_ + T)
+		
+		return hex(L_)[2:] + hex(R_)[2:]
 
-		print(L_,R_)
-		return hex(L_)[2:],hex(R_)[2:]
-
-	def decrypt(self,Ln,Rn):
-		L, R = int(Ln, 16), int(Rn, 16)
-		L_, R_ = self.H(L,R)
-		print(L_,R_)
+	def decrypt(self,ciphertext):
+		lens = int(len(ciphertext)/2)
+		L_, R_ = int(ciphertext[:lens], 16), int(ciphertext[lens:], 16)
+		
+		L_, R_ = self.H(L_, R_)
 		for _ in range(self.rounds):
 			T = self.F(L_ - R_, self.key)
-			L_, R_ = self.H(L_ - T, R_ - T)
-
-		return self.i2s((L_,R_))
+			L_, R_ = self.H_(L_ - T, R_ - T)
+		
+		bins = ["{0:#010b}".format(int(v)).replace("b","") for v in [L_,R_]]
+		return "".join(self.b2s(b) for b in bins)
 	
-	def i2s(self,ints):
-		return "".join([chr(ele) for ele in ints])
-
 	def b2s(self,bins):
 		return "".join([chr(int(ele,2)) for ele in re.findall(r"\d{8}",bins)])
 
-	def b2h(self,strs):
-		return hex(int(strs,2))
+
+
+
+class test(unittest.TestCase):
+	"""docstring for test"""
+	def test_base(self):
+		key = "ADAW2FS4242f"
+		plaintext = "abcdfedada231#"
+
+		f = Lai_Massey(key,4)
+		ciphertext = f.encrypt(plaintext)
+		plaintext_ = f.decrypt(ciphertext)
+
+		self.assertEqual(plaintext,plaintext_)
+
+	def test_new_F(self):
+		key = "123433"
+		plaintext = "abcdf"
+
+		def _f(a,keys):
+			p = a *1234 * keys
+			return p if p > 0 else -p
+
+		f = Lai_Massey(key,2,f=_f)
+		ciphertext = f.encrypt(plaintext)
+		plaintext_ = f.decrypt(ciphertext)
+
+		self.assertEqual(plaintext,plaintext_)
+
+	def test_different_types_string(self):
+		key = b'\xe4Q`\xdb!F\x0c\xfb\xbdZ\xb8?&%A\xf2'
+		plaintext = "abcdf"
+
+		f = Lai_Massey(key,2)
+		ciphertext = f.encrypt(plaintext)
+		plaintext_ = f.decrypt(ciphertext)
+
+		self.assertEqual(plaintext,plaintext_)
+
+	def test_strange_inpt1(self):
+		key = "12#A33"
+		plaintext = "@!#CASF:"
+
+		f = Lai_Massey(key,2)
+		ciphertext = f.encrypt(plaintext)
+		plaintext_ = f.decrypt(ciphertext)
+
+		self.assertEqual(plaintext,plaintext_)
+
+	def test_strange_inpt2(self):
+		key = b'\xe4Q`\xdb!F\x0c\xfb\xbdZ\xb8?&%A\xf2'
+		plaintext = b'\xe4Q`\xdb!F\x0c\xfb\xbdZ\xb8?&%A\xf2'
+
+		f = Lai_Massey(key,2)
+		ciphertext = f.encrypt(plaintext)
+		plaintext_ = f.decrypt(ciphertext)
+		plaintext_ = unhexlify(plaintext_)
+		self.assertEqual(plaintext,plaintext_)
 
 if __name__ == '__main__':
-	key = "123433"
-	plaintext = "gc"
-	f = Lai_Massey(key,2)
-	Ln,Rn = f.encrypt(plaintext)
-	print(Ln,Rn)
 
-	print(f.decrypt(Ln,Rn))
+	unittest.main()
